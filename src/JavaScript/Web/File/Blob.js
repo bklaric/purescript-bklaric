b8c540b6d22b31bc8a2fa601503bd3e208aab208
Branimir Klarić <branimir.klaric.bk@gmail.com>
2024-02-25 00:19:49 +0100

    Add Blob.type

---
+++ src/JavaScript/Web/File/Blob.js
@@ -0,0 +1,3 @@
+export function type_(blob) {
+    return blob.type
+}
--- src/JavaScript/Web/File/Blob.purs
+++ src/JavaScript/Web/File/Blob.purs
@@ -3,3 +3,5 @@ module JavaScript.Web.File.Blob where
 -- https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
 foreign import data Blob :: Type
+
+foreign import type_ :: Blob -> String
