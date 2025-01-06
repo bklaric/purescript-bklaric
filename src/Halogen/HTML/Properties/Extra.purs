77c10c34501a378c8de559292d0bfec9dab24425
Branimir Klarić <branimir.klaric.bk@gmail.com>
2025-01-06 17:24:46 +0100

    Add simplified Halogen attr'

--- src/Halogen/HTML/Properties/Extra.purs
+++ src/Halogen/HTML/Properties/Extra.purs
@@ -2,8 +2,11 @@ module Halogen.HTML.Properties.Extra where
 
 import Prelude
 
-import Halogen.HTML.Properties (IProp, class_)
-import Web.HTML.Common (ClassName(..))
+import Halogen.HTML.Properties (IProp, attr, class_)
+import Web.HTML.Common (AttrName(..), ClassName(..))
 
 class' :: forall r i. String -> IProp (class :: String | r) i
 class' = class_ <<< ClassName
+
+attr' :: forall r i. String -> String -> IProp r i
+attr' name value = attr (AttrName name) value
