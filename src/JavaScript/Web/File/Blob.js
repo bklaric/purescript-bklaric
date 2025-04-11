97c2c18b978f7786e463f09c21da35ec0f7be5e3
Branimir Klarić <branimir.klaric.bk@gmail.com>
2025-04-11 21:31:34 +0200

    Add Blob constructor

--- src/JavaScript/Web/File/Blob.js
+++ src/JavaScript/Web/File/Blob.js
@@ -1,3 +1,11 @@
+export function _new(blobParts) {
+    return function (options) {
+        return function () {
+            return new Blob(blobParts, options)
+        }
+    }
+}
+
 export function type_(blob) {
     return blob.type
 }
--- src/JavaScript/Web/File/Blob.purs
+++ src/JavaScript/Web/File/Blob.purs
@@ -1,7 +1,25 @@
-module JavaScript.Web.File.Blob where
+module JavaScript.Web.File.Blob (Blob, new, new_, type_) where
+
+import Effect (Effect)
+import Literals.Undefined (undefined)
+import Untagged.Castable (class Castable, cast)
+import Untagged.Union (type (|+|), UndefinedOr)
 
 -- https://developer.mozilla.org/en-US/docs/Web/API/Blob
 
 foreign import data Blob :: Type
+
+foreign import _new :: UndefinedOr (Array (Blob |+| String)) -> UndefinedOr {type :: UndefinedOr String} -> Effect Blob
+
+new :: forall blobParts options.
+    Castable blobParts (UndefinedOr (Array (Blob |+| String))) =>
+    Castable options (UndefinedOr {type :: UndefinedOr String}) =>
+    blobParts -> options -> Effect Blob
+new blobParts options = _new (cast blobParts) (cast options)
+
+new_ :: forall blobParts.
+    Castable blobParts (UndefinedOr (Array (Blob |+| String))) =>
+    blobParts -> Effect Blob
+new_ blobParts = new blobParts undefined
 
 foreign import type_ :: Blob -> String
